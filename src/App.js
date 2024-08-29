30ef802a0edf2fd303e50cffa4a36b314211a7cd
18936008682 <shenyaqi3434@cicdi.cn>
2024-08-29 14:23:00 +0800

    测试提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan='2'>
+                {category}
+            </th>
+        </tr>
+    );
 }
 
-export default App;
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name : <span style={{ color: 'red' }}>{product.name}</span>;
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+}
+
+function ProductTable({ products }) {
+    const rows=[];
+    let lastCategory=null;
+    products.forEach(product => {
+        if(product.category !== lastCategory){
+            rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+        }
+        rows.push(<ProductRow product={product} key={product.name} />);
+        lastCategory=product.category;
+    });
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
+    );
+}
+
+function SearchBar({ }){
+    
+}
