6bb3ac064415316bd203814ba09150c4564d1535
18936008682 <shenyaqi3434@cicdi.cn>
2024-08-29 15:42:06 +0800

    页面显示完善

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ function ProductRow({ product }) {
 }
 
 function ProductTable({ products }) {
-    const rows=[];
-    let lastCategory=null;
-    products.forEach(product => {
-        if(product.category !== lastCategory){
+    const rows = [];
+    let lastCategory = null;
+    products.forEach((product) => {
+        if (product.category !== lastCategory) {
             rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
         }
         rows.push(<ProductRow product={product} key={product.name} />);
-        lastCategory=product.category;
+        lastCategory = product.category;
     });
     return (
         <table>
@@ -44,6 +44,59 @@ function ProductTable({ products }) {
     );
 }
 
-function SearchBar({ }){
-    
+function SearchBar() {
+    return (
+        <>
+
+            <form>
+                <input type="text" placeholder="search..." />
+                <label>
+                    <input type="checkbox" />
+                    { }
+                    only show products in stocked
+
+                </label>
+            </form>
+            <div>
+                <label>
+                    <input type="checkbox" />
+                    { }
+                    111111111111111111111111111
+
+                </label>
+                <label>
+                    <input type="checkbox" />
+                    { }
+                    222222222222222222222222222
+
+                </label>
+
+            </div>
+        </>
+    );
+}
+
+function FilterableProductTable({ products }) {
+
+    return (
+        <div>
+            <SearchBar />
+            <ProductTable products={products} />
+        </div>
+    );
+}
+
+const PRODUCTS = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
+
+export default function App() {
+    return (
+        <FilterableProductTable products={PRODUCTS} />
+    );
 }
